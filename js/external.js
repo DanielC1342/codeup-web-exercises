643a242f28c1f5e4b013ea327867133b5827d65d
danielconner <metalman1342@gmail.com>
2018-03-19 11:14:57 -0500

    finished em all

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert("Welcome to my webzone!");
+
+var colour = prompt("What is your favorite color?");
+console.log("WOW! " + colour + " is my favorite color too!");
