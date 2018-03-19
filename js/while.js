59305ec28df9acbcb670c4148e366fbbee8c9969
danielconner <metalman1342@gmail.com>
2018-03-19 14:02:07 -0500

    giving up on while for now, will come back to it

--- js/while.js
+++ js/while.js
@@ -1,24 +1,23 @@
-var wile = 2;
-while (wile < 70000) {
-    console.log(wile);
-    wile = wile * 2;
-}
-
-// This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-
-do {
-    var customers = Math.floor(Math.random() * 5) + 1;
-    if (customers > allCones) {
-        console.log("I cannot sell you " + customers + " cones, I only have " + allCones);
-        break;
-    }
-    else if (allCones = 0) {
-        console.log("I sold all the cones!");
-        break;
-    }
-    else {
-        console.log("sold " + customers + " cones");
-    }
-    allCones = allCones - customers;
-} while(allCones < 0);
+// var wile = 2;
+// while (wile < 70000) {
+//     console.log(wile);
+//     wile = wile * 2;
+// }
+//
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// console.log(allCones);
+// do {
+//     var customers = Math.floor(Math.random() * 5) + 1;
+//     if (customers > allCones) {
+//         console.log("I cannot sell you " + customers + " cones, I only have " + allCones);
+//     }
+//     else if (allCones = 0) {
+//         console.log("I sold all the cones!");
+//     }
+//     else {
+//         console.log("sold " + customers + " cones");
+//     }
+//     allCones -= customers;
+//     console.log(allCones);
+// } while(allCones > -90);
