bb9f3fb7a77d32c6e98046cb700fba43a9e9552a
danielconner <metalman1342@gmail.com>
2018-03-19 15:32:30 -0500

    finished while loop

--- js/while.js
+++ js/while.js
@@ -6,19 +6,22 @@ while (wile < 70000) {
 }
 
 // // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// console.log(allCones);
-// do {
-//     var customers = Math.floor(Math.random() * 5) + 1;
-//     if (customers > allCones) {
-//         console.log("I cannot sell you " + customers + " cones, I only have " + allCones);
-//     }
-//     else if (allCones = 0) {
-//         console.log("I sold all the cones!");
-//     }
-//     else {
-//         console.log("sold " + customers + " cones");
-//     }
-//     allCones -= customers;
-//     console.log(allCones);
-// } while(allCones > -90);
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("total cones is "+allCones);
+do {
+    var customers = Math.floor(Math.random() * 5) + 1;
+    var runningCount = allCones - customers;
+    if (customers > allCones) {
+        console.log("I cannot sell you " + customers + " cones, I only have " + allCones);
+        break;
+    }
+    else if (allCones === 0) {
+        console.log("I sold all the cones!");
+        break;
+    }
+    else {
+        console.log("sold " + customers + " cones");
+    }
+    allCones = runningCount;
+    console.log(allCones+" cones left");
+} while(allCones > 0);
