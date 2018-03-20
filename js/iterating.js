4dcba057f7f407623eace2c8985838047b50b35f
danielconner <metalman1342@gmail.com>
2018-03-20 08:33:39 -0500

    iteration done

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,41 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    var names = ["Travis","Justin","Clint","Griffin"];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
+    console.log(" ");
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    console.log(" ");
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
+    console.log(" ");
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    names.forEach(function(name) {
+        console.log(name);
+    });
+    console.log(" ");
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +54,21 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function firstly(list) {
+        console.log(list[0]);
+    }
 
+    firstly(names);
+
+    function secondly(list) {
+        console.log(list[1]);
+    }
+
+    secondly(names);
+
+    function lastly(list) {
+        console.log(names[list.length - 1]);
+    }
+
+    lastly(names);
 })();
