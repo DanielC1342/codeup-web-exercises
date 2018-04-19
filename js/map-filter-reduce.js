9bc94612f0a9de066e3e0cba6b9486f41e1bfd5a
danielconner <metalman1342@gmail.com>
2018-04-19 13:54:54 -0500

    I dont really understand part 3

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,38 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+const threelang = users.filter(user => user.languages.length >= 3);
+console.log(threelang);
+
+const emails = users.map(user => user.email);
+console.log(emails);
