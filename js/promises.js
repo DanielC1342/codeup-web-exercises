a228573afc4bdb44e57c889d479f5821dc4f373b
danielconner <metalman1342@gmail.com>
2018-04-20 14:19:03 -0500

    got the wait function to work

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,14 @@
 function wait(msec) {
-    setTimeout(const timepromise = new Promise((resolve, reject)=>{
-        if(typeof msec === number)
-            resolve(`waited ${msec} milliseconds before firing`);
-    }),msec)
-
+    return new Promise((resolve, reject) => {
+        if(typeof msec === "number") {
+            setTimeout(() => {
+                resolve(`Waited ${msec/1000} seconds`);
+            }, msec);
+        }
+        else {
+            reject('Not a Number');
+        }
+    });
 }
 
-wait(3000).then(msg => {console.log(msg)});
+wait(2000).then(msg => {console.log(msg)});
