712b8f780380ac770369238db2b6ae8b9bc3e8ff
danielconner <metalman1342@gmail.com>
2018-04-23 09:29:43 -0500

    managed to work it out

--- js/promises.js
+++ js/promises.js
@@ -10,5 +10,22 @@ function wait(msec) {
         }
     });
 }
+// wait(2000).then(msg => {console.log(msg)});
 
-wait(2000).then(msg => {console.log(msg)});
+const username = "DanielC1342";
+const githubPromise = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token 49738c1549f842827da8508aa76915bca399d612'}});
+
+let r;
+
+githubPromise
+    .then(response => response.json())
+    .then(users => {
+        console.log(`last event at ${users[0].created_at}`);
+    })
+    .catch( error => {
+        console.log(error);
+    });
+
+//GET /users/:username/events/public
+
+
